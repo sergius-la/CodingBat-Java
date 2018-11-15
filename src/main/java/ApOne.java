62cfcf24fec1b8f23d4e799a530facfbc045b8e6
sergiusqa <sergius.la@gmail.com>
2018-11-15 14:20:48 -0800

    Array-3, Ap-1

--- src/main/java/ApOne.java
+++ src/main/java/ApOne.java
@@ -6,6 +6,48 @@ import org.testng.annotations.Test;
  */
 public class ApOne {
 
+    @Test
+    void test() {
+        int[] nums = {2, 2, 4, 4};
+        System.out.println(scoresAverage(nums));
+    }
+
+    /*
+        Given an array of scores, compute the int average of the first half and the second half, and return whichever is larger.
+        We'll say that the second half begins at index length/2. The array length will be at least 2.
+        To practice decomposition, write a separate helper method int average(int[] scores, int start, int end)
+        { which computes the average of the elements between indexes start..end. Call your helper method twice to implement scoresAverage().
+        Write your helper method after your scoresAverage() method in the JavaBat text area. Normally you would compute averages with doubles,
+        but here we use ints so the expected results are exact.
+
+        scoresAverage([2, 2, 4, 4]) → 4
+        scoresAverage([4, 4, 4, 2, 2, 2]) → 4
+        scoresAverage([3, 4, 5, 1, 2, 3]) → 4
+    */
+
+    public int scoresAverage(int[] scores) {
+        int firstHalf = (int) getAverage(getArrayByIndexes(scores, 0, scores.length/2));
+        int secondHalf = (int) getAverage(getArrayByIndexes(scores, scores.length/2, scores.length));
+
+        return Math.max(firstHalf, secondHalf);
+    }
+
+    private double getAverage(int[] nums) {
+        double sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum / nums.length;
+    }
+
+    private int[] getArrayByIndexes(int[] nums, int start, int end) {
+        int[] result = new int[end - start];
+        for (int i = start, j = 0; i < end; i++, j++) {
+            result[j] = nums[i];
+        }
+        return result;
+    }
+
     /*
         Given an array of scores, return true if each score is equal or greater than the one before. The array will be length 2 or more.
 
---
+++ src/main/java/ArrayThree.java
@@ -0,0 +1,162 @@
+import org.testng.annotations.Test;
+import java.util.Arrays;
+
+/**
+ * CodingBat: Array-2
+ * https://codingbat.com/java/Array-2
+ */
+public class ArrayThree {
+
+    @Test
+    void test() {
+        int[] nums1 = {5, 3, 5, 4, 5, 4, 5, 4, 3, 5, 3, 5};
+        System.out.println(Arrays.toString(fix34(nums1)));
+
+//        int[] nums2 = {1, 3, 1, 4, 4, 3, 1};
+//        System.out.println(Arrays.toString(fix34(nums2)));
+//
+//        int[] nums3 = {1, 1, 1, 1, 1};
+////        System.out.println(countClumps(nums3));
+    }
+
+
+    /*
+        Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is
+        immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's,
+        every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+
+        fix34([1, 3, 1, 4]) → [1, 3, 4, 1]
+        fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
+        fix34([3, 2, 2, 4]) → [3, 4, 2, 2]
+    */
+
+    public int[] fix34(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 3 && nums[i+1] !=4) { // Можно разбить на отдельный if и проверку по длинне массива
+                for (int j = i; j <= nums.length; j++) {
+
+                    if (j == nums.length) {
+                        for (int y = i; y > 0; y--) {
+                            if (nums[y] == 4 && nums[y-1] != 3) {
+                                int temp = nums[i+1];
+                                nums[i+1] = nums[y];
+                                nums[y] = temp;
+                            }
+                        }
+                        break;
+                    }
+
+                    if (nums[j] == 4) {
+                        int temp = nums[i+1];
+                        nums[i+1] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
+
+    /*
+        Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value. Return the number of clumps in the given array.
+
+        countClumps([1, 2, 2, 3, 4, 4]) → 2
+        countClumps([1, 1, 2, 1, 1]) → 2
+        countClumps([1, 1, 1, 1, 1]) → 1
+    */
+
+    public int countClumps(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                result++;
+                int check = nums[i];
+                for (int j = i; j <= nums.length; j++) {
+                    if (j == nums.length) {
+                        return result;
+                    }
+                    if (nums[j] != check) {
+                        i = j-1;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /*
+        Given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order.
+
+        linearIn([1, 2, 4, 6], [2, 4]) → true
+        linearIn([1, 2, 4, 6], [2, 3, 4]) → false
+        linearIn([1, 2, 4, 4, 6], [2, 4]) → true
+    */
+
+    public boolean linearIn(int[] outer, int[] inner) {
+        int check = 0;
+
+        for (int i = 0; i < inner.length; i++) {
+            int index = 0;
+            for (int j = index; j < outer.length; j++) {
+                if (inner[i] == outer[j]) {
+                    check++;
+                    index = j;
+                    break;
+                }
+            }
+        }
+        return (check == inner.length);
+    }
+
+    /*
+        Given n>=0, create an array with the pattern {1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n} (spaces added to show the grouping).
+        Note that the length of the array will be 1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2.
+
+        seriesUp(3) → [1, 1, 2, 1, 2, 3]
+        seriesUp(4) → [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+        seriesUp(2) → [1, 1, 2]
+    */
+
+    public int[] seriesUp(int n) {
+        int[] result = new int[n * (n + 1) / 2];
+        int j = result.length - 1;
+        for (int i = n; i > 0; i--) {
+            for (int y = i; y > 0; y--) {
+                result[j] = y;
+                j--;
+            }
+        }
+        return result;
+    }
+
+    /*
+        Given a non-empty array, return true if there is a place to split the array so that
+        the sum of the numbers on one side is equal to the sum of the numbers on the other side.
+
+        canBalance([1, 1, 1, 2, 1]) → true
+        canBalance([2, 1, 1, 2, 1]) → false
+        canBalance([10, 10]) → true
+    */
+
+    public boolean canBalance(int[] nums) {
+        boolean result = false;
+        int sumLeft = 0;
+        int sumRight = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumLeft += nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                sumRight += nums[j];
+            }
+            if (sumLeft == sumRight) {
+                return true;
+            } else {
+                sumRight = 0;
+            }
+        }
+        return result;
+    }
+}
+
