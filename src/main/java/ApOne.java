fbc070d37ab9fcfd2602bd5e361694fc99b749f7
sergiusqa <sergius.la@gmail.com>
2018-11-15 17:32:28 -0800

    Ap

--- src/main/java/ApOne.java
+++ src/main/java/ApOne.java
@@ -1,8 +1,6 @@
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * CodingBat: AP-1
@@ -12,8 +10,89 @@ public class ApOne {
 
     @Test
     void test() {
-        int[] a = {1, 100, 99, 100};
-        System.out.println(scores100(a));
+        String[] a = {"a", "c", "x"};
+        String[] a1 = {"a", "b", "c", "x", "z"};
+
+        System.out.println(commonTwo(a, a1));
+    }
+
+    /*
+        Start with two arrays of strings, a and b, each in alphabetical order, possibly with duplicates.
+        Return the count of the number of strings which appear in both arrays.
+        The best "linear" solution makes a single pass over both arrays, taking advantage of the fact that they are in alphabetical order.
+
+        commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) → 2
+        commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
+        commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3
+    */
+
+    public int commonTwo(String[] a, String[] b) {
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i].equals(b[j])) {
+                    result.add(a[i]);
+                }
+            }
+        }
+        return result.size();
+    }
+
+    /*
+        We'll say that a positive int divides itself if every digit in the number divides into the number evenly.
+        So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly. We'll say that 0 does not divide into anything evenly,
+        so no number with a 0 digit divides itself. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+        dividesSelf(128) → true
+        dividesSelf(12) → true
+        dividesSelf(120) → false
+    */
+
+    public boolean dividesSelf(int n) {
+        boolean result = true;
+        int num = n;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 0 || n % digit != 0) {
+                return false;
+            }
+            num = num / 10;
+        }
+        return result;
+    }
+
+    /*
+        Given an array of strings, return a new array containing the first N strings. N will be in the range 1..length.
+
+        wordsFront(["a", "b", "c", "d"], 1) → ["a"]
+        wordsFront(["a", "b", "c", "d"], 2) → ["a", "b"]
+        wordsFront(["a", "b", "c", "d"], 3) → ["a", "b", "c"]
+    */
+
+    public String[] wordsFront(String[] words, int n) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = words[i];
+        }
+        return result;
+    }
+
+    /*
+        Given an array of strings, return the count of the number of strings with the given length.
+
+        wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+        wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+        wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+    */
+
+    public int wordsCount(String[] words, int len) {
+        int result = 0;
+        for (String i : words) {
+            if (i.length() == len) {
+                result++;
+            }
+        }
+        return result;
     }
 
     /*
